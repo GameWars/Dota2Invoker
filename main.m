943811a0ebebbf2456e1cb46012ce902ea9fd823
Du Cong <dcnocomment@gmail.com>
2015-01-19 14:54:38 +0800

    First commit

---
+++ main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Dota2Invoker
+//
+//  Created by Du Cong on 4/12/13.
+//  Copyright (c) 2013 Du Cong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
