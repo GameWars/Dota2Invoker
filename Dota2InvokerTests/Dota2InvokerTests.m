1d05bb82cdfcce082bf8448c94a534bd5a4b6e0a
Du Cong <dcnocomment@gmail.com>
2015-01-19 15:09:34 +0800

    initial commit

---
+++ Dota2InvokerTests/Dota2InvokerTests.m
@@ -0,0 +1,34 @@
+//
+//  Dota2InvokerTests.m
+//  Dota2InvokerTests
+//
+//  Created by Du Cong on 4/12/13.
+//  Copyright (c) 2013 Du Cong. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Dota2InvokerTests : XCTestCase
+
+@end
+
+@implementation Dota2InvokerTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
